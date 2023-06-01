7f84e53a939c76ca41e7006b2c66b99f29ce24c9
Lesiba Moekwa <lesiba.moekwa@dadevs.co.za>
2023-06-01 11:19:31 +0200

    Initial Commit

---
+++ MovieApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApplication
+//
+//  Created by DA MAC M1 139 on 2023/06/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
